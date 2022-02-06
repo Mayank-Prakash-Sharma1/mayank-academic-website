d405d775af85755c6f9d02ba0d622025231e72f3
Mayank-Prakash-Sharma1 <mayank.vision18@gmail.com>
2022-02-06 22:15:44 +0530

    sdsd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,3 +45,28 @@ function CopyToClipboard(containerid) {
     /* Alert the copied text */
     alert("Copied the text: " + copyText.value);
   }
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    let disqus = document.getElementById('disqus_thread');
+
+    let remove_ads = setInterval(() => {
+        let iframes = document.getElementsByTagName('iframe');
+
+        for (var iframe in iframes) {
+            if (typeof iframes[iframe].src === 'undefined') {
+                continue;
+            }
+
+            if (iframes[iframe].src.match(/(ads-iframe)|(disqusads)/gi)) {
+                iframes[iframe].style.display = 'none';
+                disqus.style.width = '100%';
+            }
+        }
+    }, 500);
+
+    setTimeout(function () {
+        clearInterval(remove_ads);
+    }, 5000);
+});
